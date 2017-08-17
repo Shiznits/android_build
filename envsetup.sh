42c5ec61a0695fa21a30ec417f480fec4e10406b
Luca Stefani <luca020400@lineageos.org>
2017-08-17 21:21:23 +0200

    build: Add Custom build support

--- envsetup.sh
+++ envsetup.sh
@@ -130,6 +130,13 @@ function check_product()
         echo "Couldn't locate the top of the tree.  Try setting TOP." >&2
         return
     fi
+    if (echo -n $1 | grep -q -e "^aosp_") ; then
+        CUSTOM_BUILD=$(echo -n $1 | sed -e 's/^aosp_//g')
+    else
+        CUSTOM_BUILD=
+    fi
+    export CUSTOM_BUILD
+
         TARGET_PRODUCT=$1 \
         TARGET_BUILD_VARIANT= \
         TARGET_BUILD_TYPE= \
@@ -609,6 +616,7 @@ function lunch()
     fi
 
     check_product $product
+
     TARGET_PRODUCT=$product \
     TARGET_BUILD_VARIANT=$variant \
     TARGET_PLATFORM_VERSION=$version \
